dfacb10d849ef6f5a9355afa4c18b6103351ac70
tosh <toshism@gmail.com>
2019-05-28 12:06:57 -0500

    gergo symbols layer done

--- keymap.c
+++ keymap.c
@@ -49,26 +49,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_TAB,         KC_BSPACE,      KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
 
                        KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
-                       KC_PGUP,       LT(1, KC_ENTER),         LT(2, KC_SPACE)
+                       KC_PGUP,
+		       LT(1, KC_ENTER),         LT(2, KC_SPACE)
         ),
 
   [1] = LAYOUT_ergodox(
-                       KC_ESCAPE,       KC_F1,         KC_F2,         KC_F3,         KC_F4,           KC_F5,         KC_TRANSPARENT,
-                       KC_GRAVE,        KC_EXLM,       KC_AT,         KC_LCBR,       KC_RCBR,         KC_PIPE,       KC_PGUP,
-                       KC_DLR,          KC_EQUAL,      KC_MINUS,      KC_LPRN,       KC_RPRN,         KC_KP_ASTERISK,
-                       KC_CIRC,         KC_PERC,       KC_UNDS,       KC_LBRACKET,   KC_RBRACKET,     KC_AMPR,       KC_PGDOWN,
-                       KC_TRANSPARENT,  KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,KC_PLUS,
+                       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                       KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_LCBR,        KC_RCBR,        KC_PIPE,        KC_TRANSPARENT,
+                       KC_TRANSPARENT, KC_HASH,        KC_DOLLAR,      KC_LPRN,        KC_RPRN,        KC_GRAVE,
+                       KC_TRANSPARENT, KC_PERC,        KC_CIRC,        KC_LBRACKET,    KC_RBRACKET,    KC_TILDE,       KC_TRANSPARENT,
+                       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
-                                                                               KC_BSPACE,     KC_LSHIFT,     KC_F13,
+                                                                               KC_SCOLON,     KC_EQUAL,      KC_TRANSPARENT,
 
-                       KC_TRANSPARENT,  KC_F6,         KC_F7,         KC_F8,         KC_F9,           KC_F10,        KC_F11,
-                       KC_TRANSPARENT,  KC_MINUS,      KC_7,          KC_8,          KC_9,            KC_ASTR,       KC_F12,
-                                        KC_DOT,        KC_4,          KC_5,          KC_6,            KC_KP_MINUS,   KC_F11,
-                       KC_TRANSPARENT,  KC_0,          KC_1,          KC_2,          KC_3,            KC_KP_PLUS,    KC_F10,
-                                                       KC_KP_0,       KC_0,          KC_DOT,          KC_EQUAL,      KC_TRANSPARENT,
+                       KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                       KC_TRANSPARENT,  KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+                                        KC_KP_PLUS,     KC_KP_MINUS,    KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_PERCENT,  KC_QUOTE,
+                       KC_TRANSPARENT,  KC_AMPERSAND,   KC_EQUAL,       KC_COMMA,       KC_KP_DOT,      KC_KP_SLASH,    KC_MINUS,
+                                                        KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
 
                        KC_TRANSPARENT,KC_TRANSPARENT,KC_TRANSPARENT,
-                       KC_F14,KC_TRANSPARENT,KC_ENTER
+                       KC_TRANSPARENT,
+		       KC_EQUAL, KC_SCOLON
         ),
 
   [2] = LAYOUT_ergodox(
